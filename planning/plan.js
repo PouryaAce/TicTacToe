0e6bddd394ccb520c5d1360c27446d66a3617096
PouryaAce <pouriaghafour@gmail.com>
2022-02-02 09:55:59 +1100

    innnitial commit

---
+++ planning/plan.js
@@ -0,0 +1,36 @@
+//////////////////  PROJECT 1 \\\\\\\\\\\\\\\\\\\\\
+//   P           L            A            N     \\
+
+//1. need to create html file and use semantic tags
+// create main and section with list of 9 ids
+//.give each id a width and heaight and border so we can see them
+//2.give the section a display of grid
+//section {
+//display:grid , grid-template-columns , ...
+//}
+//3.target each box of grid individually and when the box is clicked
+
+//need to create a function to target all boxes within parent element
+
+// function(event)
+
+//4.when its clicked in an empty appear X in the box
+
+//function xO(event)
+//   var secClicked = event.target;
+//   if (secClicked.tagName === "SEC") {
+//     secClicked.textcontent = "X";
+//   }
+// }
+// var Parent = document.querySelector("Parent");
+// Parent.addEventListener("click", xO);
+
+//5.next time its clicked in an empty appear O in the box
+
+///?????
+
+//6.you can only use X or O one time on each round
+
+//7.use clicks switch X and O
+
+//8.if we get 3 similar items in 3 rows same player wins
