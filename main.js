64fc40e69f2c554719d66bf2cf0a9cdcf8f6f7ae
PouryaAce <pouriaghafour@gmail.com>
2022-02-03 11:45:48 +1100

    innnitial-commit

---
+++ main.js
@@ -0,0 +1,178 @@
+var boxTxt1 = document.querySelector("#box-1");
+var boxTxt2 = document.querySelector("#box-2");
+var boxTxt3 = document.querySelector("#box-3");
+var boxTxt4 = document.querySelector("#box-4");
+var boxTxt5 = document.querySelector("#box-5");
+var boxTxt6 = document.querySelector("#box-6");
+var boxTxt7 = document.querySelector("#box-7");
+var boxTxt8 = document.querySelector("#box-8");
+var boxTxt9 = document.querySelector("#box-9");
+var win = false;
+var disPlay = document.querySelector(".display-player");
+var numClicked = 0;
+var zeus = document.querySelector(".display-win-zeus").textContent;
+var kratos = document.querySelector(".display-win-kratos").textContent;
+var winnerDisplay1 = document.querySelector(".winner-kratos-left");
+var winnerDisplay2 = document.querySelector(".winner-zeus-right");
+var section = document.querySelector(".container");
+var draw = document.querySelector(".draw");
+function xOMaker(event) {
+  var boxClicked = event.target;
+  if (boxClicked.tagName === "DIV") {
+    if (boxClicked.textContent === "" && numClicked % 2) {
+      boxClicked.textContent = "☇";
+      numClicked = numClicked + 1;
+      disPlay.textContent = "KRATΩS";
+
+      if (
+        boxTxt1.textContent == "☇" &&
+        boxTxt2.textContent == "☇" &&
+        boxTxt3.textContent == "☇"
+      ) {
+        winnerDisplay2.textContent = "WINS";
+        win = true;
+        section.removeEventListener("click", xOMaker);
+      } else if (
+        boxTxt4.textContent == "☇" &&
+        boxTxt5.textContent == "☇" &&
+        boxTxt6.textContent == "☇"
+      ) {
+        winnerDisplay2.textContent = "WINS";
+        win = true;
+        section.removeEventListener("click", xOMaker);
+      } else if (
+        boxTxt7.textContent == "☇" &&
+        boxTxt8.textContent == "☇" &&
+        boxTxt9.textContent == "☇"
+      ) {
+        winnerDisplay2.textContent = "WINS";
+        win = true;
+        section.removeEventListener("click", xOMaker);
+      } else if (
+        boxTxt1.textContent == "☇" &&
+        boxTxt5.textContent == "☇" &&
+        boxTxt9.textContent == "☇"
+      ) {
+        winnerDisplay2.textContent = "WINS";
+        win = true;
+        section.removeEventListener("click", xOMaker);
+      } else if (
+        boxTxt3.textContent == "☇" &&
+        boxTxt5.textContent == "☇" &&
+        boxTxt7.textContent == "☇"
+      ) {
+        winnerDisplay2.textContent = "WINS";
+        win = true;
+        section.removeEventListener("click", xOMaker);
+      } else if (
+        boxTxt1.textContent == "☇" &&
+        boxTxt4.textContent == "☇" &&
+        boxTxt7.textContent == "☇"
+      ) {
+        winnerDisplay2.textContent = "WINS";
+        win = true;
+        section.removeEventListener("click", xOMaker);
+      } else if (
+        boxTxt2.textContent == "☇" &&
+        boxTxt5.textContent == "☇" &&
+        boxTxt8.textContent == "☇"
+      ) {
+        winnerDisplay2.textContent = "WINS";
+        win = true;
+        section.removeEventListener("click", xOMaker);
+      } else if (
+        boxTxt3.textContent == "☇" &&
+        boxTxt6.textContent == "☇" &&
+        boxTxt9.textContent == "☇"
+      ) {
+        winnerDisplay2.textContent = "WINS";
+        win = true;
+        section.removeEventListener("click", xOMaker);
+      }
+      if (numClicked == 9 && win == false) {
+        draw.textContent = "DRAW";
+        console.log(numClicked + "X");
+      }
+    } else if (boxClicked.textContent === "") {
+      boxClicked.textContent = "Ω";
+      numClicked = numClicked + 1;
+      disPlay.textContent = "ZEU☇S";
+      console.log("o" + numClicked);
+    }
+    if (numClicked == 9 && win == false) {
+      draw.textContent = "DRAW";
+    }
+    if (
+      boxTxt1.textContent == "Ω" &&
+      boxTxt2.textContent == "Ω" &&
+      boxTxt3.textContent == "Ω"
+    ) {
+      winnerDisplay1.textContent = "WINS";
+      win = true;
+      section.removeEventListener("click", xOMaker);
+    } else if (
+      boxTxt4.textContent == "Ω" &&
+      boxTxt5.textContent == "Ω" &&
+      boxTxt6.textContent == "Ω"
+    ) {
+      winnerDisplay1.textContent = "WINS";
+      win = true;
+      section.removeEventListener("click", xOMaker);
+    } else if (
+      boxTxt7.textContent == "Ω" &&
+      boxTxt8.textContent == "Ω" &&
+      boxTxt9.textContent == "Ω"
+    ) {
+      winnerDisplay1.textContent = "WINS";
+      win = true;
+      section.removeEventListener("click", xOMaker);
+    } else if (
+      boxTxt1.textContent == "Ω" &&
+      boxTxt5.textContent == "Ω" &&
+      boxTxt9.textContent == "Ω"
+    ) {
+      winnerDisplay1.textContent = "WINS";
+      win = true;
+      section.removeEventListener("click", xOMaker);
+    } else if (
+      boxTxt3.textContent == "Ω" &&
+      boxTxt5.textContent == "Ω" &&
+      boxTxt7.textContent == "Ω"
+    ) {
+      winnerDisplay1.textContent = "WINS";
+      win = true;
+      section.removeEventListener("click", xOMaker);
+    } else if (
+      boxTxt1.textContent == "Ω" &&
+      boxTxt4.textContent == "Ω" &&
+      boxTxt7.textContent == "Ω"
+    ) {
+      winnerDisplay1.textContent = "WINS";
+      win = true;
+      section.removeEventListener("click", xOMaker);
+    } else if (
+      boxTxt2.textContent == "Ω" &&
+      boxTxt5.textContent == "Ω" &&
+      boxTxt8.textContent == "Ω"
+    ) {
+      winnerDisplay1.textContent = "WINS";
+      win = true;
+      section.removeEventListener("click", xOMaker);
+    } else if (
+      boxTxt3.textContent == "Ω" &&
+      boxTxt6.textContent == "Ω" &&
+      boxTxt9.textContent == "Ω"
+    ) {
+      winnerDisplay1.textContent = "WINS";
+      win = true;
+      section.removeEventListener("click", xOMaker);
+    }
+  }
+}
+
+section.addEventListener("click", xOMaker);
+
+var button = document.querySelector(".reset");
+button.addEventListener("click", function () {
+  window.location.reload();
+});
--- plan.js
+++ plan.js
@@ -2,12 +2,15 @@
 //   P           L            A            N     \\
 
 //1. need to create html file and use semantic tags
-// create main and section with list of 9 ids
-//.give each id a width and heaight and border so we can see them
+// create main and section with 9 divs in it
+
+//.give each a width and heaight and border so we can see them
+
 //2.give the section a display of grid
 //section {
 //display:grid , grid-template-columns , ...
 //}
+
 //3.target each box of grid individually and when the box is clicked
 
 //need to create a function to target all boxes within parent element
@@ -16,24 +19,29 @@
 
 //4.when its clicked in an empty appear X in the box
 
-//function xO(event)
+//function clickXMaker(event)
 //   var secClicked = event.target;
-//   if (secClicked.tagName === "SEC") {
+//   if (secClicked.tagName === "SECTION") {
 //     secClicked.textcontent = "X";
 //   }
 // }
-// var Parent = document.querySelector("Parent");
-// Parent.addEventListener("click", xO);
+// var container = document.querySelector(".container");
+// container.addEventListener("click", clickXMaker );
 
 //5.next time its clicked in an empty appear O in the box
 
-///?????
+//if textcontent of first click === "X"
+//run function clickOMaker
 
 //6.you can only use X or O one time on each round
 
 //7.use clicks switch X and O
 
-// need to difine player-X
-// need to define player-O
+// need to difine player-1 = 'O'
+// need to define player-2 = 'X'
 
-//8.if we get 3 similar items in 3 rows same player wins
+//8.if we get 3 similar items in 3 rows and cross ways same player wins
+// assign var winner =
+
+//9.if we have winner add another semantic tag to change its textconent to
+//`Winner is ${}
